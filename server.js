4d836d33fdec23477520301b8c8b46d7990eaddf
Drew <drew.c.pierson@gmail.com>
2020-07-04 18:23:40 -0500

    second commit

---
+++ server.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Drew',
+            email: 'drew@gmail.com',
+            password: '1987',
+            entries: 12,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.json(database.users)
+})
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[1].email &&
+        req.body.password === database.users[1].password) {
+        res.json('success')
+    } else {
+        res.status(400).json('error logging in')
+    }
+})
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length-1])
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id){
+            found = true;
+            return res.json(user);
+        }
+    })
+    if (!found) {
+        res.status(400).json("User not found.");
+    }
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    })
+    if (!found) {
+        res.status(400).json('User not found.')
+    }
+})
+
+app.listen(port)
+
+/*
+
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+
+*/
